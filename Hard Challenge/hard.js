b6508e02b991daa28475a7d5c5b06900bca968ac
dmhebron <dmhebron21@gmail.com>
2022-09-19 11:15:31 -0400

    completed hard challenge

---
+++ Hard Challenge/hard.js
@@ -0,0 +1,19 @@
+// Addtion Function
+let add = function (num1, num2) {
+    console.log (num2 + num1);
+};
+
+// Subtraction Function
+let subtract = function (num1, num2) {
+    console.log (num2 - num1);
+};
+
+// Multiplication Function 
+let multiply = function (num1, num2) {
+    console.log (num2 * num1);
+};
+
+// Division Function
+let divide = function (num1, num2) {
+    console.log (num2 / num1);
+};

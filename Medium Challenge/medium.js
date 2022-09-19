ad4f0a81eda8def34ba53ce9bbb5954c6238f200
dmhebron <dmhebron21@gmail.com>
2022-09-19 00:17:12 -0400

    completed medium challenge

---
+++ Medium Challenge/medium.js
@@ -0,0 +1,10 @@
+const text = prompt("How do you feel about JavaScript?");
+
+if (text == text.toLowerCase ()) {
+    alert ("The user is whispering.");}
+
+    else if (text == text.toUpperCase ()) 
+    { alert ("The user is shouting!");}
+
+    else 
+    { alert ("The user is speaking normally.");}

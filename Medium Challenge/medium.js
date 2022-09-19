ab8c96c374a6245a5fa5e15cd4607d316e64dcb7
dmhebron <dmhebron21@gmail.com>
2022-09-19 00:20:02 -0400

    changed the alert message

--- Medium Challenge/medium.js
+++ Medium Challenge/medium.js
@@ -1,10 +1,10 @@
 const text = prompt("How do you feel about JavaScript?");
 
 if (text == text.toLowerCase ()) {
-    alert ("The user is whispering.");}
+    alert ("The user is whispering.".toLowerCase());}
 
     else if (text == text.toUpperCase ()) 
-    { alert ("The user is shouting!");}
+    { alert ("The user is shouting!".toUpperCase());}
 
     else 
     { alert ("The user is speaking normally.");}
